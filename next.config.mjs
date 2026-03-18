13a9416cd790a306293e1008f42d3dc47fc436df
John <riauz24032003@gmail.com>
2026-03-18 13:48:08 +0500

    new changesss

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'standalone',
+    allowedDevOrigins: ['10.10.115.21']
+}
 
 export default nextConfig
